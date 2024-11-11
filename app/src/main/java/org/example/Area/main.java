2d5a31ace489f723987d73877f70230d2f87b316
Ittani Maemo <ittanim@gmail.com>
2024-11-11 22:52:35 +0200

    update

---
+++ app/src/main/java/org/example/Area/main.java
@@ -0,0 +1,24 @@
+package org.example.Area;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[])
+    {
+        Wall wall = new Wall();
+        Scanner val = new Scanner(System.in);
+
+        System.out.println("Please type the width of the wall below");
+        double width =val.nextDouble();
+       wall.setWidth(width);
+        System.out.println("Please type the height of the wall below");
+        double height= val.nextDouble();
+        wall.setHeight(height);
+
+        double totalArea = wall.getArea();
+
+        System.out.println("Total Area = " + totalArea +"m^2");
+
+        val.close();
+    }
+}
