bd7d71a70b4199a8c5fcd18c5d3990b38926052a
Ittani Maemo <ittanim@gmail.com>
2024-12-11 14:39:26 +0200

    InheritanceChallenge

--- app/src/main/java/org/example/Challenges/main.java
+++ app/src/main/java/org/example/Challenges/main.java
@@ -3,11 +3,18 @@ package org.example.Challenges;
 public class main {
     public static void main(String agrs[])
     {
-        SalariedEmployee salariedEmployee = new SalariedEmployee(543534l,"2024-11-25","Thompho","1956-12-06","2035-11-25",2200.78,false);
+        SalariedEmployee salariedEmployee = new SalariedEmployee(543534l,"2024-11-25","Thompho","1956-12-06","2035-11-25",23412344.89,false);
         HourlyEmployee hourlyEmployee = new HourlyEmployee(6545321l,"2016-10-12","Thulani","1993-11-23","2046-11-12",700.67);
         System.out.println("The employee details: \n");
+        System.out.println("Annual Employee ");
         System.out.println( "Employee id:" + salariedEmployee.getEmployeeId() + "\nName: " +
-                salariedEmployee.getName() + "\nHireDate: " + salariedEmployee.getHireDate() + "\nPay Rate Per Hour R" + salariedEmployee.getAnnualSalary() +
+                salariedEmployee.getName() + "\nHireDate: " + salariedEmployee.getHireDate() + "\nAnnual pay  R" + salariedEmployee.getAnnualSalary() +
          "\nBirth-Date: " + salariedEmployee.getBirthDate());
+
+        System.out.println("\nHourly Employee ");
+        System.out.println( "Employee id:" + hourlyEmployee.getEmployeeId() + "\nName: " +
+                hourlyEmployee.getName() + "\nHireDate: " + hourlyEmployee.getHireDate() + "\nPay Rate Per Hour R" + hourlyEmployee.getHourlypayRate() +
+                "\nBirth-Date: " + hourlyEmployee.getBirthDate());
     }
+
 }
